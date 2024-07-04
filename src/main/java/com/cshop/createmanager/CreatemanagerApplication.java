7eaf2457943ff0c240a89464f3c1a271c76f44fb
onkarkshirsagar1234 <onkark657@gmail.com>
2024-07-04 16:21:02 +0530

    initail commit

---
+++ src/main/java/com/cshop/createmanager/CreatemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.cshop.createmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatemanagerApplication.class, args);
+	}
+
+}
